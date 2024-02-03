4d45736961724db33f0db8c2efbc4f529c008c6c
Hanna Gaudasińska-Zapaśnik <HGZdevi@gmail.com>
2024-02-03 22:00:50 +0100

    daisyUI init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  plugins: [daisyui],
   theme: {
     extend: {},
   },
-
-  // plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#4970A2",
+          secondary: "#85BFE1",
+          accent: "palegreen",
+          neutral: "#0a2d3e",
+          "base-100": "#fff",
+          info: "#006cd5",
+          success: "#58a089",
+          warning: "#ffb300",
+          error: "#cc0000",
+        },
+      },
+    ],
+  },
 };
